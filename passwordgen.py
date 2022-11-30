24bcf3fed1414fb012539a94723af57fa06ec331
Voue <41804856+CherryVoue@users.noreply.github.com>
2022-11-30 01:12:18 -0500

    Add passgen

---
+++ passwordgen.py
@@ -0,0 +1,21 @@
+import string
+import random
+
+length = int(input("Desired length of password (must be at least 4: "))
+
+reqs = random.choice(string.ascii_lowercase) + random.choice(string.ascii_uppercase) + random.choice(string.digits) + random.choice(string.punctuation)
+
+ascii_chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+
+passw = ''
+
+pass_chars = list(reqs)
+for i in range(length-4):
+	pass_chars += random.choice(ascii_chars)
+
+while len(pass_chars) > 0:
+	temp = random.choice(pass_chars)
+	passw += temp
+	pass_chars.remove(temp)
+	
+print(f'Generated password: {passw}')
